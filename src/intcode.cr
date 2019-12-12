acefbe0ef329792162fd71be9bf79dfcfaeadd03
Jim Fitzpatrick <fitzpatrick.jim@gmail.com>
2019-12-12 08:51:58 -0500

    Intcode enhancements
    - Implement relative mode
    - Implement adjust-relative-base instruction

--- src/intcode.cr
+++ src/intcode.cr
@@ -4,6 +4,7 @@ end
 class Intcode
   def initialize(memory : Array(Int32), input_values : Array(Int32))
     @pc = 0
+    @rb = 0
     @memory = memory
     @input_values = input_values
     @outputs = [] of Int32
@@ -11,15 +12,23 @@ class Intcode
   end
 
   private def param(n)
-    @memory[@pc + n]
+    mod = 100 * 10 ** n
+    mode = (read_mem(@pc) % mod) // (mod / 10)
+    case mode
+    when 0 then read_mem @pc + n
+    when 1 then read_mem @pc + n
+    when 2 then @rb + read_mem(@pc + n)
+    else -1
+    end
   end
 
   private def value(n)
     mod = 100 * 10 ** n
-    mode = (@memory[@pc] % mod) // (mod / 10)
+    mode = (read_mem(@pc) % mod) // (mod / 10)
     case mode
-    when 0 then @memory[param(n)]
+    when 0 then read_mem param(n)
     when 1 then param(n)
+    when 2 then read_mem param(n)
     else -1
     end
   end
@@ -37,7 +46,7 @@ class Intcode
   end
 
   def step
-    case @memory[@pc] % 100
+    case read_mem(@pc) % 100
     when 1 then add
     when 2 then mul
     when 3 then input
@@ -46,12 +55,30 @@ class Intcode
     when 6 then jump_if_false
     when 7 then less_than
     when 8 then equals
+    when 9 then adjust_rel_base
     when 99 then halt
     end
   end
 
+  private def read_mem(i)
+    result = @memory[i]?
+    if result.nil?
+      0
+    else
+      result
+    end
+  end
+
+  private def write_mem(i, val)
+    if i >= @memory.size
+      @memory += Array.new(i - @memory.size + 1, 0)
+    end
+    @memory[i] = val
+  end
+
   private def binop
-    @memory[param(3)] = yield value(1), value(2)
+    val = yield value(1), value(2)
+    write_mem param(3), val
     @pc += 4
   end
 
@@ -65,7 +92,8 @@ class Intcode
 
   private def input
     begin
-      @memory[param(1)] = @input_values.shift
+      val = @input_values.shift
+      write_mem param(1), val
       @pc += 2
     rescue IndexError
       raise IntcodeMissingInputError.new
@@ -99,6 +127,11 @@ class Intcode
 
   private def equals
     binop { |a, b| a == b ? 1 : 0 }
+  end
+
+  private def adjust_rel_base
+    @rb += value(1)
+    @pc += 2
   end
 
   private def halt
