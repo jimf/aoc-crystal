1c676660e80d3fae79b346296bd3e69a10c03879
Jim Fitzpatrick <fitzpatrick.jim@gmail.com>
2019-12-16 11:06:11 -0500

    Day 2019-11

--- src/advent2019_11.cr
+++ src/advent2019_11.cr
@@ -1,8 +1,45 @@
+require "./intcode.cr"
+
 module Advent2019_11
   extend self
 
+  private class Turtle
+    getter x : Int32
+    getter y : Int32
+
+    def initialize
+      @x = 0
+      @y = 0
+      @angle = 0
+    end
+
+    def right
+      @angle = (@angle + 90) % 360
+    end
+
+    def left
+      3.times do
+        right
+      end
+    end
+
+    def forward
+      case @angle
+      when 0
+        @y -= 1
+      when 90
+        @x += 1
+      when 180
+        @y += 1
+      when 270
+        @x -= 1
+      end
+    end
+  end
+
+
   private def parse(input)
-    input.split(',').map { |n| n.to_i }
+    input.split(',').map { |n| n.to_i64 }
   end
 
   def name
@@ -10,10 +47,101 @@ module Advent2019_11
   end
 
   def part1(input)
-    nil
+    board = {} of Tuple(Int32, Int32) => Int64
+    painted = {} of Tuple(Int32, Int32) => Int32
+    inputs = [] of Int64
+    vm = Intcode.new parse(input), inputs
+    robot = Turtle.new
+
+    while !vm.halted
+      pos = {robot.x, robot.y}
+      inputs << board.fetch(pos, 0.to_i64)
+      painted[pos] = painted.fetch(pos, 0) + 1
+
+      while !vm.halted
+        begin
+          vm.step
+        rescue IntcodeMissingInputError
+          break
+        end
+      end
+
+      board[pos] = vm.outputs[-2]
+
+      if vm.outputs[-1] == 0
+        robot.left
+      else
+        robot.right
+      end
+
+      robot.forward
+    end
+
+    painted.size
   end
 
   def part2(input)
-    nil
+    board = {} of Tuple(Int32, Int32) => Int64
+    painted = {} of Tuple(Int32, Int32) => Int32
+    inputs = [] of Int64
+    vm = Intcode.new parse(input), inputs
+    robot = Turtle.new
+
+    board[{0, 0}] = 1.to_i64
+
+    while !vm.halted
+      pos = {robot.x, robot.y}
+      inputs << board.fetch(pos, 0.to_i64)
+      painted[pos] = painted.fetch(pos, 0) + 1
+
+      while !vm.halted
+        begin
+          vm.step
+        rescue IntcodeMissingInputError
+          break
+        end
+      end
+
+      board[pos] = vm.outputs[-2]
+
+      if vm.outputs[-1] == 0
+        robot.left
+      else
+        robot.right
+      end
+
+      robot.forward
+    end
+
+    min_x = 0
+    min_y = 0
+    max_x = 0
+    max_y = 0
+
+    board.each do |key, val|
+      x, y = key
+
+      min_x = [min_x, x].min
+      min_y = [min_y, y].min
+      max_x = [max_x, x].max
+      max_y = [max_y, y].max
+    end
+
+    deltax = 0 - min_x
+    deltay = 0 - min_y
+
+    result = Array.new max_y - min_y + 1, do
+      Array.new max_x - min_x + 1, ' '
+    end
+
+    board.each do |key, value|
+      x, y = key
+
+      if value == 1
+        result[deltay + y][deltax + x] = '#'
+      end
+    end
+
+    '\n' + result.join('\n') { |row| row.join }
   end
 end
